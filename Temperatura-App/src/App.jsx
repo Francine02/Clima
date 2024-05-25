a45ff55779cc2a4f8a1b649317e01d908fab2203
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-05-25 16:38:21 -0300

    first commit

---
+++ Temperatura-App/src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav } from './Nav';
+
+function App() {
+  return (
+    <div id='theme'> 
+    <Nav/>
+    </div>
+  )
+}
+
+export default App
---
+++ Temperatura-App/src/Nav.jsx
@@ -0,0 +1,9 @@
+export function Nav () {
+    return (
+        <div className="d-flex justify-content-center pt-1" id="nav">
+        <label htmlFor="search" className="m-4">
+            <input type="text" id="search" placeholder="Insira a sua cidade..." className="p-2 text-center" />
+        </label>
+        </div>
+    )
+}
