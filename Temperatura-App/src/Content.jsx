8d623013359faea95934d4b35152fbef172ae700
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-06-03 15:36:43 -0300

    Fix: Corrigido a quantidade de requisição que estava sendo enviada para a api

--- Temperatura-App/src/Content.jsx
+++ Temperatura-App/src/Content.jsx
@@ -1,5 +1,5 @@
 import sunIcon from "./img/sol.png";
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 
 export function Content () {
     const [weatherData, setWeatherData] = useState(null);
@@ -16,49 +16,56 @@ export function Content () {
             }
             const apiFind = await apiResponse.json();
             setWeatherData(apiFind);
+            console.log(apiFind)
         } catch (error) {
             console.error("Erro ao buscar dados da API", error);
         }
     }
 
-    document.addEventListener("keypress", ({key}) => {
-        if(key === "Enter") {
-            search()
-        } 
-    }) //Fim da parte de pesquisar e enviar como requerimento para a api
+    useEffect(() => { //Um hook que permite executar efeitos colaterais em componentes funcionais. Aqui, é usado para adicionar e remover um ouvinte de evento.
+        const handleKeyPress = ({ key }) => {
+            if (key === "Enter") {
+                search();
+            }
+        };
+        document.addEventListener("keypress", handleKeyPress);
+        return () => {
+            document.removeEventListener("keypress", handleKeyPress);
+        };
+    }, [search]);//Adiciona o ouvinte de eventos keypress quando o componente é montado e o remove quando é desmontado.
 
     return (
-        <>
-        <div className="row px-5 pt-5 pb-4" id="content">
-            <div className="col-6 px-md-5 d-flex justify-content-center">
-                <div>
-                    <h1 id="title">{weatherData ? weatherData.name : ""}</h1>
-                    <p><img src="https://img.icons8.com/?size=100&id=17768&format=png&color=000000" alt="local"/>{weatherData ? weatherData.sys.country : ""}</p>
+        <div style={{ display: weatherData ? 'block' : 'none' }}>
+            <div className="row px-5 pt-5 pb-4" id="content">
+                <div className="col-6 px-md-5 d-flex justify-content-center">
+                    <div>
+                        <h1 id="title">{weatherData ? weatherData.name : ""}</h1>
+                        <p><img src="https://img.icons8.com/?size=100&id=17768&format=png&color=000000" alt="local"/>{weatherData ? weatherData.sys.country : ""}</p>
 
-                    <h2>{weatherData ? weatherData.main.temp.toFixed(0) + "°C" : ""}</h2>
+                        <h2>{weatherData ? weatherData.main.temp.toFixed(0) + "°C" : ""}</h2>
+                    </div>
+                </div>
+                
+                <div className="col-6 d-flex justify-content-center">
+                    <img src={sunIcon} alt="clima" className="img-fluid" id="clima"/>
                 </div>
             </div>
-            
-            <div className="col-6 d-flex justify-content-center">
-                <img src={sunIcon} alt="clima" className="img-fluid" id="clima"/>
+
+            <div className="row px-5 d-flex justify-content-center text-center">
+                <div className="col-6 m-lg-4 m-2 d-flex justify-content-center" id="info">
+                    <div className="p-1">
+                        <img src="https://img.icons8.com/?size=100&id=4iKImf8GW8Sy&format=png&color=000000" alt="humidade" className="img-fluid"/>
+                        <p className="pt-1">{weatherData ? weatherData.main.humidity + "%" : ""}</p>
+                    </div>
+                </div>
+
+                <div className="col-6 m-lg-4 m-2 d-flex justify-content-center" id="info">
+                    <div className="p-1">
+                        <img src="https://img.icons8.com/?size=100&id=jjb7r7iIe27C&format=png&color=000000" alt="vento" className="img-fluid"/>
+                        <p className="pt-1">{weatherData ? Math.round(weatherData.wind.speed) + "km/h" : ""}</p>
+                    </div>
+                </div>
             </div>
         </div>
-
-        <div className="row px-5 d-flex justify-content-center text-center">
-            <div className="col-6 m-lg-4 m-2 d-flex justify-content-center" id="info">
-                <div className="p-1">
-                    <img src="https://img.icons8.com/?size=100&id=4iKImf8GW8Sy&format=png&color=000000" alt="humidade" className="img-fluid"/>
-                    <p className="pt-1">{weatherData ? weatherData.main.humidity + "%" : ""}</p>
-                </div>
-            </div>
-
-            <div className="col-6 m-lg-4 m-2 d-flex justify-content-center" id="info">
-                <div className="p-1">
-                    <img src="https://img.icons8.com/?size=100&id=jjb7r7iIe27C&format=png&color=000000" alt="vento" className="img-fluid"/>
-                    <p className="pt-1">{weatherData ? Math.round(weatherData.wind.speed) + "km/h" : ""}</p>
-                </div>
-            </div>
-        </div>
-        </>
     )
 }
--- Temperatura-App/src/Nav.jsx
+++ Temperatura-App/src/Nav.jsx
@@ -2,7 +2,7 @@ export function Nav () {
     return (
         <div className="d-flex justify-content-center pt-2" id="nav">
         <label htmlFor="search" className="m-4">
-            <input type="text" id="search" placeholder="Insira a sua cidade" className="p-2 text-center" />
+            <input type="text" id="search" placeholder="Insira a sua cidade" className="p-2 text-center"/>
         </label>
         </div>
     )
