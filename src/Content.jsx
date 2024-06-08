c09d9c85e00f1ccaf522b2e62bf55002a683142a
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-06-08 11:36:59 -0300

    Style: Ajeitando as medias queries

--- src/Content.jsx
+++ src/Content.jsx
@@ -79,8 +79,8 @@ export function Content () {
     }
     return (
         <div className="container" style={{ display: weatherData ? 'block' : 'none' }}>
-            <div className="row px-5 pt-3 pb-5" id="content">
-                <div className="col-6 px-md-5 d-flex justify-content-center">
+            <div className="row px-lg-5 px-1 pt-3 pb-5" id="content">
+                <div className="col-6 px-lg-5 px-3 d-flex justify-content-start">
                     <div className="pt-4">
                         <h1 id="title">{weatherData ? weatherData.name : ""}</h1>
                         <p><img src="https://img.icons8.com/?size=100&id=17768&format=png&color=000000" alt="local"/>{weatherData ? weatherData.sys.country : ""}</p>
@@ -89,21 +89,21 @@ export function Content () {
                     </div>
                 </div>
                 
-                <div className="col-6 px-md-5 d-flex justify-content-center">
+                <div className="col-6 px-lg-5 px-3 d-flex justify-content-end">
                     <img src={weatherData ? icon() : ""} alt="clima" className="img-fluid" id="clima"/>
                     
                 </div>
             </div>
 
-            <div className="row px-5 d-flex justify-content-center text-center">
-                <div className="col-6 m-lg-4 m-2 d-flex justify-content-center" id="info">
+            <div className="row px-lg-5 d-flex justify-content-center text-center">
+                <div className="col-6 m-lg-4 m-3 d-flex justify-content-center" id="info">
                     <div className="p-1">
                         <img src="https://img.icons8.com/?size=100&id=4iKImf8GW8Sy&format=png&color=000000" alt="humidade" className="img-fluid"/>
                         <p className="pt-1">{weatherData ? weatherData.main.humidity + "%" : ""}</p>
                     </div>
                 </div>
 
-                <div className="col-6 m-lg-4 m-2 d-flex justify-content-center" id="info">
+                <div className="col-6 m-lg-4 m-3 d-flex justify-content-center" id="info">
                     <div className="p-1">
                         <img src="https://img.icons8.com/?size=100&id=jjb7r7iIe27C&format=png&color=000000" alt="vento" className="img-fluid"/>
                         <p className="pt-1">{weatherData ? Math.round(weatherData.wind.speed) + "km/h" : ""}</p>
